2fc4727a212d5b507a5d0eb9dd67fd33b64aaa62
tamaki uno <t.uno@keio.jp>
2025-06-27 18:11:14 +0900

    ポップアップ機能を新規作成し、ログイン、サインアップ、ログアウトフォームを統合。関連するHTML、CSS、JavaScriptファイルを追加・修正。

---
+++ public/modules/popup.js
@@ -0,0 +1,46 @@
+'use strict';
+
+console.log('popup.js loaded');
+
+export default function switchForm() {
+    const path = window.location.pathname; // 現在のパスを取得
+    console.log('Current path:', path);
+    const popup = document.querySelector('.popup');
+    // ログイン、サインアップ、ログアウト以外のパスではエラー
+    if (path !== '/login' && path !== '/signup' && path !== '/logout') {
+        console.error('Invalid path for popup:', path);
+        return;
+    }
+    popup.querySelectorAll('form').forEach(form => form.style.display = 'none');
+    let form;
+    switch (path) {
+        case '/login':
+            form = popup.querySelector('form.login-form');
+            popup.querySelector('h2.popup-title').textContent = 'Dive back in!';
+            break;
+        case '/signup':
+            form = popup.querySelector('form.signup-form');
+            popup.querySelector('h2.popup-title').textContent = 'Dive in!';
+            break;
+        case '/logout':
+            form = popup.querySelector('form.logout-form');
+            popup.querySelector('h1').textContent = 'Logout';
+            break;
+        default:
+            console.error('Unknown path:', path);
+            return;
+    }
+    form.style.display = 'flex';
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        // フォームの送信処理をここに追加
+        console.log('Form submitted');
+    });
+    form.querySelector('p a').addEventListener('click', (event) => {
+        event.preventDefault();
+        // 再読込せず、ポップアップのフォームを切り替える
+        switchForm(event.target.getAttribute('href'));
+        window.history.pushState({}, '', event.target.getAttribute('href')); // URLを更新
+
+    });
+}
--- public/script.js
+++ public/script.js
@@ -1,65 +1,77 @@
 'use strict';
 
+console.log('script.js loaded');
+
+// import switchForm from './components/popup.js';
+import switchForm from './modules/popup.js';
 
 const API_BASE_PATH = '/api/v0';
 
 document.addEventListener('DOMContentLoaded', () => {
     // const API_BASE_URL = 'localhost:3000/api/v0';
 
+
     const path = window.location.pathname;
     if (path === '/login' || path === '/signup' || path === '/logout') {
         // ログイン、サインアップ、ログアウトのページではAPIベースパスを設定
         window.apiBasePath = API_BASE_PATH;
         // html/popup.htmlを読み込む
-        fetch('/html/popup.html')
+        // fetch('/html/popup.html')
+        // fetch('/components/popup.html')
+        fetch('/modules/popup.html')
             .then(response => response.text())
             .then(popupHtml => {
                 // ポップアップのHTMLをbodyに挿入
                 document.body.insertAdjacentHTML('beforeend', popupHtml);
-                // ポップアップの初期化
-                const popup = document.querySelector('.popup');
-                // ポップアップの閉じるボタンにイベントリスナーを追加
-                popup.querySelector('.close-popup-button').addEventListener('click', () => {
-                    popup.remove(); // ポップアップをDOMから削除
-                    window.location.href = '/'; // ホームページにリダイレクト
-                });
-                const displayForm = (querySelector) => {
-                    popup.querySelectorAll('form').forEach(form => form.style.display = formDisplay);
-                    // popup.querySelector(querySelector).style.display = 'flex';
-                    popup.querySelector(querySelector).style.display = 'block';
-                };
-                
-                switch (path) {
-                    case '/login':
-                        popup.querySelector('h2.popup-title').textContent = 'Dive back in!';
-                        // popup.querySelector('form.login-form').style.display = 'block';
-                        displayForm('form.login-form');
-                        break;
-                    case '/signup':
-                        // popup.querySelector('form').action = '/signup';
-                        popup.querySelector('h2.popup-title').textContent = 'Dive in!';
-                        // popup.querySelector('form.signup-form').style.display = 'block';
-                        displayForm('form.signup-form');
-                        break;
-                    case '/logout':
-                        popup.querySelector('h1').textContent = 'Logout';
-                        // popup.querySelector('form.logout-form').style.display = 'block';
-                        displayForm('form.logout-form');
-                        break;
-                    default:
-                        console.error('Unknown path:', path);
-                        return;
-                }
-                // フォームの送信イベントを処理
-                popup.querySelector('form').addEventListener('submit', (event) => {
-                    event.preventDefault();
-                    // フォームの送信処理をここに追加
-                    console.log('Form submitted');
-                });
+                switchForm(); // ポップアップの初期化
             }).catch(error => {
                 console.error('Error loading popup HTML:', error);
             });
     }
-
-
+    //     // ポップアップの初期化
+    //     const popup = document.querySelector('.popup');
+    //     // popup.style.display = 'flex'; // ポップアップを表示
+    //     popup.style.display = 'block'; // ポップアップを表示
+    //     // ポップアップの閉じるボタンにイベントリスナーを追加
+    //     popup.querySelector('.close-popup-button').addEventListener('click', () => {
+    //         // popup.remove(); // ポップアップをDOMから削除
+    //         popup.style.display = 'none'; // ポップアップを非表示
+    //         window.location.href = '/'; // ホームページにリダイレクト
+    //     });
+    //     // ポップアップのフォームを表示
+    //     let form;
+    //     const displayForm = (querySelector) => {
+    //         popup.querySelectorAll('form').forEach(form => form.style.display = 'none');
+    //         form = popup.querySelector(querySelector);
+    //         form.style.display = 'flex';
+    //     };
+    //     switch (path) {
+    //         case '/login':
+    //             popup.querySelector('h2.popup-title').textContent = 'Dive back in!';
+    //             displayForm('form.login-form');
+    //             break;
+    //         case '/signup':
+    //             popup.querySelector('h2.popup-title').textContent = 'Dive in!';
+    //             displayForm('form.signup-form');
+    //             break;
+    //         case '/logout':
+    //             popup.querySelector('h1').textContent = 'Logout';
+    //             displayForm('form.logout-form');
+    //             break;
+    //         default:
+    //             console.error('Unknown path:', path);
+    //             return;
+    //     }
+    //     // フォームの送信イベントを処理
+    //     form.addEventListener('submit', (event) => {
+    //         event.preventDefault();
+    //         // フォームの送信処理をここに追加
+    //         console.log('Form submitted');
+    //     });
+    //     // 
+    //     form.querySelector('p a').addEventListener('click', (event) => {
+    //         event.preventDefault();
+    //         // 再読込せず、ポップアップのフォームを切り替える
+    //     });
+    // }
 });
